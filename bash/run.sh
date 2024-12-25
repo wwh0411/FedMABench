db89b6d199b320b09b7833e06f1aeb7f6529635f
wwh0411 <2438208264@qq.com>
2024-12-25 22:07:24 +0800

    u

--- bash/run.sh
+++ bash/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # script1.sh
 
+
+
+
 bash run_central_internvl2_2.sh /ailab/user/wangwenhao/ms-swift-main/output/high/alg5_train_5000_v1.json
 #bash run_central_internvl2_2.sh /ailab/user/wangwenhao/ms-swift-main/output/high/alg2_train_5000_v1.json
